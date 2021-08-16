cee8c0715ca1d348f9bfb033cbdf6b9959d1a222
Vjacheslav250 <johndoe@example.comjohndoe@example.com>
2021-08-16 22:27:00 +0300

    Сделана cтилизация секции about

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,46 @@
-(function($) {})(jQuery);
+(function ($) {
+  let $navbar = $('#header-navbar');
+
+
+  const headerClassOnScroll = 'header-navbar--scrolled';
+  const headerScrollTrigger = 100;
+
+  $(window).on('scroll', () => {
+    $(window).scrollTop() > headerScrollTrigger
+      ? $navbar.addClass(headerClassOnScroll)
+      : $navbar.removeClass(headerClassOnScroll);
+    back2Top($(window).scrollTop());
+  });
+
+
+  $navbar.onePageNav({
+    currentClass: 'active',
+    scrollSpeed: 750,
+    easing: 'swing',
+    filter: ':not(.navbar-brand)'
+  });
+
+  let windowWidth = $(window).width();
+
+  $(window).on('resize', () => (windowWidth = $(window).width()));
+
+  let $navbarLink = $('#header-navbar-collapse a');
+  let $navbarToggler = $('.navbar-toggler');
+  let $headerNavbarCollapse = $('#header-navbar-collapse');
+
+  $navbarLink.on('click', () => {
+    if (windowWidth < 992) {
+      $navbarToggler.addClass('collapsed');
+      $headerNavbarCollapse.removeClass('show');
+    }
+  });
+
+  AOS.init({
+    disable: 'mobile',
+    duration: 600,
+    easing: 'ease-in-sine'
+  });
+
+})(jQuery);
+
+
